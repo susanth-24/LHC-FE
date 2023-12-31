69e06ab0fea96b4512b43e5620e673bd851b5daf
susanth-lab <68644335+susanth-lab@users.noreply.github.com>
2023-12-31 08:26:11 +0530

    update v2.4

--- src/components/GeneratePdf/main.jsx
+++ src/components/GeneratePdf/main.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment, useEffect,useState } from 'react';
-import { PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
+import React, { Fragment, useEffect } from 'react';
+import { PDFViewer } from '@react-pdf/renderer';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
@@ -13,42 +13,17 @@ const PdfViewer = () => {
 
   useEffect(() => {
     dispatch(getRequest(id));
-  }, []);
+  }, []); 
 
   // const filteredRequest = requests?.filter(request => request?._id === id);
   console.log(request);
-  const [isSmallDevice, setIsSmallDevice] = useState(false);
-
-  useEffect(() => {
-    checkWindowWidth();
-    window.addEventListener('resize', checkWindowWidth);
-
-    return () => {
-      window.removeEventListener('resize', checkWindowWidth);
-    };
-  }, []);
-
-  const checkWindowWidth = () => {
-    const { innerWidth } = window;
-    setIsSmallDevice(innerWidth < 700);
-  };
+  
 
   return (
     <Fragment>
-      {!isSmallDevice ?
-        <>
-          <PDFViewer style={{ width: '100%', height: '100vh' }}>
-            <MyDocument data={request} />
-          </PDFViewer>
-        </>
-        : <>
-          <PDFDownloadLink document={<MyDocument />} fileName={`${request?._id}.pdf`}>
-            {({ blob, url, loading, error }) =>
-              loading ? 'Loading document...' : 'Download now!'
-            }
-          </PDFDownloadLink>
-        </>}
-
+      <PDFViewer style={{ width: '100%', height: '100vh' }}>
+        <MyDocument data={request} />
+      </PDFViewer>
     </Fragment>
   );
 };
